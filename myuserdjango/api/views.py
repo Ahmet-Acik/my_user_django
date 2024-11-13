f2c058075dab49b96ead6a37f336ab9ede08ed02
drahmetacik <a.acik@icloud.com>
2024-11-13 17:58:56 +0000

    add user retrieval API endpoint and configure URL routing

--- myuserdjango/api/views.py
+++ myuserdjango/api/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import User
+from .serializer import UserSerializer
+
 
 # Create your views here.
+@api_view(['GET'])
+def get_user(request):
+    # users = User.objects.all()
+    # serializer = UserSerializer(users, many=True)
+    # return Response(serializer.data)
+    return Response(
+        UserSerializer(
+            {
+                "name": "John Doe",
+                "age": 30,
+                "email": "john.doe@example.com",
+                "password": "securepassword123",
+            }
+        ).data, status=status.HTTP_200_OK
+    )
